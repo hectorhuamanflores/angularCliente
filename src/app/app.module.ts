f1ae7e2e3c455683b812c1cff9f22312b1ca1b68
hector <hector.10.fisi@gmail.com>
2019-02-05 20:47:22 -0500

    usando web service de cliente con angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,35 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ClientesComponent } from './clientes/clientes.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { DirectivaComponent} from './directiva/directiva.component'
+import { ClienteService}  from "./clientes/cliente.service";
+import { RouterModule, Routes} from "@angular/router";
+//para usar con spring
+import { HttpClientModule} from "@angular/common/http";
+
+const routes: Routes = [
+  {path: '',redirectTo: '/clientes',pathMatch:'full'},
+  {path: 'directivas',component:DirectivaComponent},
+  {path: 'clientes',component:ClientesComponent},
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ClientesComponent,
+    HeaderComponent,
+    FooterComponent,
+    DirectivaComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    RouterModule.forRoot(routes)
   ],
-  providers: [],
+  providers: [ClienteService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/directiva/directiva.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-directiva',
+  templateUrl: './directiva.component.html',
+})
+export class DirectivaComponent {
+
+  listaCurso: string[] = ['TypeScrip','JavaScript','Java SE','C#','PHP'];
+}
